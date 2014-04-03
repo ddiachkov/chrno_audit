06738740334c0cc74413ab68d2cd33aa46949e58
Denis Diachkov <d.diachkov@gmail.com>
2014-04-04 02:33:40 +0400

    Added support for pg JSON columns

--- app/models/chrno_audit/audit_record.rb
+++ app/models/chrno_audit/audit_record.rb
@@ -14,10 +14,10 @@ class ChrnoAudit::AuditRecord < ActiveRecord::Base
   belongs_to :auditable, :polymorphic => true
 
   # Изменения
-  serialize :diff
+  serialize :diff unless columns_hash[ "diff" ].type == :json
 
   # Контекст
-  serialize :context
+  serialize :context unless columns_hash[ "context" ].type == :json
 
   # Возвращает записи для заданного типа сущности.
   scope :for_type, -> *types { where( auditable_type: types.map { |t| t.class.model_name })}
